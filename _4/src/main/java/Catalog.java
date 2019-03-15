338f35d11e60e2aea916ce1f95de1bf153285d6f
Valeriu Moise <valeriu.moise1010@yahoo.com>
2019-03-15 18:42:03 +0200

    method signatures for catalog

--- _4/src/main/java/Catalog.java
+++ _4/src/main/java/Catalog.java
@@ -1,2 +1,22 @@
-public class Catalog {
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Catalog implements Serializable {
+
+    List<Graph> graphList = new LinkedList<Graph>();
+    String path;
+
+    public Catalog(String path) {
+        this.path = path;
+    }
+
+    public void add (Graph graph){
+
+    }
+
+    public void open(String graphName){
+
+    }
+
 }
