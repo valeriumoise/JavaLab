d912ac4c58c272f3aee20aef68c836b6228816ac
OnateK <ketano1711@gmail.com>
2019-03-15 18:45:24 +0200

    Graph class

--- _4/src/main/java/Graph.java
+++ _4/src/main/java/Graph.java
@@ -1,2 +1,40 @@
 public class Graph {
+
+    private String name;
+    private String imagePath;
+    private String tgfPath;
+
+    public Graph(){
+
+    }
+
+    public Graph(String name, String imagePath, String tgfPath) {
+        this.name = name;
+        this.imagePath = imagePath;
+        this.tgfPath = tgfPath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getTgfPath() {
+        return tgfPath;
+    }
+
+    public void setTgfPath(String tgfPath) {
+        this.tgfPath = tgfPath;
+    }
 }
