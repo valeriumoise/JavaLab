a80c2a7285a2f3a76d180e769a10d2e32bf670e1
Valeriu <valeriu.moise1010@yahoo.com>
2019-04-05 18:26:38 +0300

    construuctors getters and setters

--- _8/src/main/java/model/Graph.java
+++ _8/src/main/java/model/Graph.java
@@ -1,4 +1,19 @@
 package model;
 
+import java.util.List;
+
 public class Graph {
+    List<Edge> edges;
+
+    public Graph(List<Edge> edges) {
+        this.edges = edges;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public void setEdges(List<Edge> edges) {
+        this.edges = edges;
+    }
 }
