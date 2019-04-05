7e30c136137644c42dff59fbaa9f4e45ef2b4f0e
Valeriu <valeriu.moise1010@yahoo.com>
2019-04-05 18:55:41 +0300

    Player and Game code

--- _8/src/main/java/model/Player.java
+++ _8/src/main/java/model/Player.java
@@ -1,4 +1,72 @@
 package model;
 
-public class Player {
+public class Player implements Runnable{
+    private String name;
+    private Game game;
+    private Graph graph;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public Player(String name, Game game, Graph graph) {
+        this.name = name;
+        this.game = game;
+        this.graph = graph;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Graph getGraph() {
+        return graph;
+    }
+
+    public void setGraph(Graph graph) {
+        this.graph = graph;
+    }
+
+    private boolean play() throws InterruptedException {
+        Board board = game.getBoard();
+        if (board.isEmpty()) { return false; }
+        graph.addEdge( board.extract() );
+        Thread.sleep(THINKING_TIME); //declare this constant
+        if (graph.isSpanningTree()) {
+            game.setWinner(this);
+        }
+        return true;
+    }
+
+    public void run() {
+
+    }
+    // implement the run() method, that will repeatedly extract edges
+    // implement the toString() method
+
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", game=" + game +
+                ", graph=" + graph +
+                '}';
+    }
+
+    private final static int THINKING_TIME = 2000;
+
+
 }
